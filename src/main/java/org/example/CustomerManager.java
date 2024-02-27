1e9b53909e530cb9533986dae885d42c946bb9a6
Alperen Turhan <alp-er-99@hotmail.com>
2024-02-28 01:40:52 +0300

    Gün 2 ödev

---
+++ src/main/java/org/example/CustomerManager.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Customer has been added");
+    }
+    public void Remove(){
+        System.out.println("Customer has been removed");
+    }
+    public void Update(){
+        System.out.println("Customer has been update");
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+    }
+}
